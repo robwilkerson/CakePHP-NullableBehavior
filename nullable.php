20c7514ae26124734e98e94b0b35c065d436cf5a
Rob Wilkerson <rob@robwilkerson.org>
2011-10-24 10:56:30 -0300

    Handling model aliases and better empty string recognition.

--- nullable.php
+++ nullable.php
@@ -1,27 +1,25 @@
 <?php
 
 class NullableBehavior extends ModelBehavior {
-	/**
-	 * function beforeSave
-	 * 
-	 * Looks for nullable fields in the schema and replaces empty string values for those fields
-	 * with NULL values. This is helpful as hell when foreign key values are nullable lest you
-	 * get lots of key constraint errors.
-	 *
-	 * @param		model		The model object to be saved.
-	 * @return	void
-	 */
-	function beforeSave ( $model ) {
-		$schema = $model->schema();
+  /**
+   * function beforeSave
+   * 
+   * Looks for nullable fields in the schema and replaces empty string values for those fields
+   * with NULL values. This is helpful as hell when foreign key values are nullable lest you
+   * get lots of key constraint errors.
+   *
+   * @param   model   The model object to be saved.
+   * @return  void
+   */
+  function beforeSave( $model ) {
+    $schema = $model->schema();
 
-		foreach ( $schema as $field => $metadata ) {
-			if ( $metadata['null'] ) {
-				if ( isset ( $model->data[$model->name][$field] ) && $model->data[$model->name][$field] === '' ) {
-					$model->data[$model->name][$field] = null;
-				}
-			}
-		}
-	}
+    foreach( $schema as $field => $metadata ) {
+      if( isset( $model->data[$model->alias][$field] ) && $metadata['null'] ) {
+        if( $model->data[$model->alias][$field] == '' ) {
+          $model->data[$model->alias][$field] = null;
+        }
+      }
+    }
+  }
 }
-
-?>
